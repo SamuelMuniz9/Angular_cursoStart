ba496136edc4e10eca19f3f0204caa735de00c81
Samuel Muniz <msamuelmuniz@gmail.com>
2026-01-22 16:04:54 -0300

    primeiro commit

---
+++ app-treino-marra/src/app/app.config.ts
@@ -0,0 +1,17 @@
+import { ApplicationConfig, NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { FormsModule } from '@angular/forms';//importação do modulo de formulários
+
+@NgModule({
+  imports: [FormsModule],//importação do modulo de formulários
+})
+export class AppModule {}// configuração do modulo principal da aplicação
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes)
+  ]
+};
---
+++ app-treino-marra/src/app/tarefa/tarefa.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+// Componente Tarefa sendo criado aqui 
+
+@Component({
+  selector: 'app-tarefa',//nome do componente
+  imports: [FormsModule, CommonModule],//importações necessárias
+  templateUrl: './tarefa.html',//caminho do template html
+  styleUrl: './tarefa.css',//caminho do css
+})
+
+
+// trabalhando com o componente tarefe, ele vira uma classe
+export class Tarefa {
+  novaTarefa: String = '';
+  listaTarefas: String[] = [  'Praticar Angular', 'Estudar Angular', 'Finalizar projeto'];
+
+  //função para adicionar tarefa, configurando a função em si 
+  adicionarTarefa() {
+    if (this.novaTarefa.trim() !== '') {//verifica se a nova tarefa não está vazia
+      this.listaTarefas.push(this.novaTarefa.trim());//adiciona a nova tarefa na lista
+      this.novaTarefa = '';//limpa o campo de entrada
+    }
+
+}
+}
